de424e66bddb93ffd1caabe734155e7cc8351b06
Sad-Pc <Sad-Pc@DESKTOP-Q522DFP>
2021-08-24 07:55:14 -0300

    Add project files.

---
+++ 01-ByteBank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaCorrente = new ContaCorrente();
+
+            contaCorrente.titular = "Gabriela";
+            contaCorrente.numeroAgencia = 0182;
+            contaCorrente.numeroConta = 1587412;
+            contaCorrente.saldo = 100.00;
+
+            Console.WriteLine(contaCorrente.titular);
+            Console.WriteLine(contaCorrente.numeroAgencia);
+            Console.WriteLine(contaCorrente.numeroConta);
+            Console.WriteLine(contaCorrente.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}

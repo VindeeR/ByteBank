a44b456936f1b5e0aed4bad82c0c326d8e5a2f8f
VindeeR <erick.lopes@gmail.com>
2021-09-30 07:48:36 -0300

    cleaning project

--- 01-ByteBank/Program.cs
+++ 01-ByteBank/Program.cs
@@ -1,24 +1,41 @@
-﻿using System;
+﻿using ByteBank.Funcionarios;
+using System;
 
-namespace _01_ByteBank
+namespace ByteBank
 {
     class Program
     {
         static void Main(string[] args)
         {
-            ContaCorrente contaCorrente = new ContaCorrente();
 
-            contaCorrente.titular = "Gabriela";
-            contaCorrente.numeroAgencia = 0182;
-            contaCorrente.numeroConta = 1587412;
-            contaCorrente.saldo = 100.00;
-
-            Console.WriteLine(contaCorrente.titular);
-            Console.WriteLine(contaCorrente.numeroAgencia);
-            Console.WriteLine(contaCorrente.numeroConta);
-            Console.WriteLine(contaCorrente.saldo);
-
+            CaclcularBonificacao();
             Console.ReadLine();
+
+        }
+
+        public static void CaclcularBonificacao()
+        {
+
+            GerenciadorBonificacao gerenciadorBonificacao = new GerenciadorBonificacao();
+
+            Designer pedro = new Designer("546.879.157-20");
+            pedro.Nome = "Pedro";
+
+            Diretor roberta = new Diretor("852.654.754-62");
+            roberta.Nome = "Roberta";
+
+            Auxiliar igor = new Auxiliar("987.456.321-34");
+            igor.Nome = "Igor";
+
+            GerenteDeConta camila = new GerenteDeConta("253.653.154-91");
+            camila.Nome = "Camila";
+
+            gerenciadorBonificacao.Registrar(pedro);
+            gerenciadorBonificacao.Registrar(roberta);
+            gerenciadorBonificacao.Registrar(igor);
+            gerenciadorBonificacao.Registrar(camila);
+
+            Console.WriteLine("Total de bonificação do mês " + gerenciadorBonificacao.GetTotalBonioficacao());
         }
     }
 }
